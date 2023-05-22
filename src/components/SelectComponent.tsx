75e5824031b5464c680975f66491059ecc71b6ba
Maria22 <m.ushakova22@gmail.com>
2023-05-22 11:57:26 +0200

    feat: add effects to select form

--- src/components/SelectComponent.tsx
+++ src/components/SelectComponent.tsx
@@ -1,5 +1,5 @@
 import { Select, SelectItem, Image } from "@mantine/core";
-import { FC, useContext } from "react";
+import { FC, useContext, useState } from "react";
 import { FilterContext } from "../pages/App/utils/context";
 import { ActionsTypes } from "../pages/App/utils/types";
 
@@ -9,6 +9,7 @@ type SelectType = {
 
 const SelectComponent: FC<SelectType> = ({ data }) => {
   const { state, dispatch } = useContext(FilterContext);
+  const [isOpened, setOpened] = useState(false);
 
   function handleChange(value: string | null) {
     dispatch({ type: ActionsTypes.changeIndustry, payload: value });
@@ -19,12 +20,42 @@ const SelectComponent: FC<SelectType> = ({ data }) => {
       data-elem="industry-select"
       data={data?.length ? [...data] : []}
       placeholder="Выберете отрасль"
-      rightSection={<Image width={24} height={24} src="/down.svg" />}
+      rightSection={isOpened ? <Image width={24} height={24} src="/upArrow.svg" /> : <Image width={24} height={24} src="/down.svg" />}
       rightSectionWidth={50}
-      styles={{ rightSection: { pointerEvents: "none" }, input: { height: "42px" } }}
+      searchable
+      nothingFound="No options"
       value={state.industry}
       fz={"1.2rem"}
       onChange={(value) => handleChange(value)}
+      onDropdownOpen={() => setOpened(true)}
+      onDropdownClose={() => setOpened(false)}
+      styles={(theme) => ({
+        rightSection: { pointerEvents: "none" },
+        input: {
+          height: "42px",
+          border: isOpened ? `1px solid ${theme.colors.blue[1]}` : `1px solid ${theme.colors.grey[2]}`,
+          '&:hover': {
+            border: `1px solid ${theme.colors.blue[1]}`
+          }
+        },
+        dropdown: {
+          fontSize: '0.85rem'
+        },
+        item: {
+          '&[data-selected]': {
+            '&, &:hover': {
+              width: "97%",
+              backgroundColor: theme.colors.blue[1],
+              color: theme.white,
+            },
+          },
+          '&[data-hovered]': {
+            width: "97%",
+            backgroundColor: theme.colors.blue[1],
+            color: theme.white,
+          },
+        },
+      })}
     />
   );
 };
